d30fcdf3e5dc79e12108423a63e9e3d21d9c707a
Welliton Fernandes Leal <wellfernandes@me.com>
2022-11-09 12:01:10 -0300

    added function that receives commands and fetches ips by host

--- project-2/internal/app/app.go
+++ project-2/internal/app/app.go
@@ -1,6 +1,11 @@
 package app
 
-import "github.com/urfave/cli"
+import (
+	"fmt"
+	"github.com/urfave/cli"
+	"log"
+	"net"
+)
 
 // Generate will return the command line application ready to run
 func Generate() *cli.App {
@@ -8,6 +13,35 @@ func Generate() *cli.App {
 	app := cli.NewApp()
 	app.Name = "Command Line Application"
 	app.Usage = "Find IP and Servers on the Internet"
-
+	// Receive commands in a slice
+	app.Commands = []cli.Command{
+		{
+			Name:  "ip",
+			Usage: "Search/find IPs on the internet",
+			Flags: []cli.Flag{
+				cli.StringFlag{
+					Name:  "host",
+					Value: "github.com/wellfernandes",
+				},
+			},
+			Action: findIps,
+		},
+	}
 	return app
+}
+
+func findIps(c *cli.Context) {
+
+	host := c.String("host")
+
+	// using package net
+	ips, err := net.LookupIP(host)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Prints the ips found
+	for _, ip := range ips {
+		fmt.Println(ip)
+	}
 }
